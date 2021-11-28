72eae3a54474d558f7f3597142f47fa79e27db4f
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2021-11-28 19:08:52 +0000

    Removing stationarity from data

--- dataloading.py
+++ dataloading.py
@@ -6,6 +6,8 @@ from perceval.backends.core.github import GitHub, CATEGORY_PULL_REQUEST
 
 from config import GITHUB_API_TOKEN, ELASTICSEARCH_HOST
 
+OWNER: str = "microsoft"
+REPOSITORY: str = "vscode"
 PULL_REQUEST_INDEX: str = "pull-requests-v2"
 
 
@@ -22,23 +24,22 @@ def create_index(elastic_search: Elasticsearch):
 
 
 def index_pull_request(elastic_search: Elasticsearch, pull_request_data: dict):
+    pull_request_id: str = str(pull_request_data['number'])
     try:
-        elastic_search.index(index=PULL_REQUEST_INDEX, document=pull_request_data)
+        elastic_search.index(index=PULL_REQUEST_INDEX, id=pull_request_id, document=pull_request_data)
     except elasticsearch.exceptions.RequestError:
-        print("Could not store pull request %d " % pull_request_data['number'])
+        print("Could not store pull request %s " % pull_request_id)
 
 
 def main():
-    owner: str = "microsoft"
-    repository: str = "vscode"
-    days_in_year: int = 365
-    factor: int = 1
     new_index: bool = True
 
+    days_in_year: int = 365
+    factor: int = 1
     from_date: datetime = datetime.now() - timedelta(days=(factor + 1) * days_in_year)
     to_date: datetime = datetime.now() - timedelta(days=factor * days_in_year)
 
-    pull_requests: GitHub = GitHub(owner=owner, repository=repository,
+    pull_requests: GitHub = GitHub(owner=OWNER, repository=REPOSITORY,
                                    api_token=GITHUB_API_TOKEN, sleep_for_rate=True)
     elastic_search: Elasticsearch = elasticsearch.Elasticsearch(ELASTICSEARCH_HOST)
     if new_index and not create_index(elastic_search):
--- devanalysis.py
+++ devanalysis.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from typing import List, Any
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 import elasticsearch
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from elasticsearch import Elasticsearch
+from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.stattools import adfuller
 
 from config import ELASTICSEARCH_HOST
 from dataloading import PULL_REQUEST_INDEX
@@ -38,9 +40,9 @@ def do_query_with_aggregation(elastic_search: Elasticsearch, aggregation_name: s
     return result_dataframe
 
 
-def get_merges_performed(elastic_search: Elasticsearch, user_login: str,
+def get_merges_performed(es: Elasticsearch, user_login: str,
                          calendar_interval: str = "month") -> pd.DataFrame:
-    result_dataframe: pd.DataFrame = do_query_with_aggregation(elastic_search, MERGES_PERFORMED_COLUMN, query={
+    result_dataframe: pd.DataFrame = do_query_with_aggregation(es, MERGES_PERFORMED_COLUMN, query={
         "bool": {
             "must": {
                 "match": {"merged_by.login": user_login}
@@ -78,9 +80,9 @@ def get_merge_requests(elastic_search: Elasticsearch, user_login: str,
     return result_dataframe
 
 
-def get_requests_merged(elastic_search: Elasticsearch, user_login: str,
+def get_requests_merged(es: Elasticsearch, user_login: str,
                         calendar_interval: str = "month"):
-    result_dataframe: pd.DataFrame = do_query_with_aggregation(elastic_search, MERGES_SUCCESSFUL_COLUMN, query={
+    result_dataframe: pd.DataFrame = do_query_with_aggregation(es, MERGES_SUCCESSFUL_COLUMN, query={
         "bool": {
             "must": [
                 {
@@ -112,52 +114,81 @@ def plot_dataframe(consolidated_dataframe: pd.DataFrame, user_login: str):
 
 def plot_offered_vs_given(consolidated_dataframe: pd.DataFrame, user_login: str):
     plt.figure(figsize=(12, 6))
-    sns.jointplot(
-        x=MERGES_PERFORMED_COLUMN, y=MERGES_SUCCESSFUL_COLUMN,
-        edgecolor="white",
-        data=consolidated_dataframe,
-    )
+    merges_performed: np.ndarray = consolidated_dataframe[MERGES_PERFORMED_COLUMN].values.reshape(-1, 1)
+    merges_successful: np.ndarray = consolidated_dataframe[MERGES_SUCCESSFUL_COLUMN].values.reshape(-1, 1)
+    linear_regression: LinearRegression = LinearRegression()
+    linear_regression.fit(merges_performed, merges_successful)
+    predicted_merges_successful: np.ndarray = linear_regression.predict(merges_performed)
+
+    plt.scatter(merges_performed, merges_successful)
+    plt.plot(merges_performed, predicted_merges_successful, color="red")
+
     plt.xlabel("Performed")
-    plt.ylabel("Requested")
+    plt.ylabel("Successful")
     plt.title("User %s" % user_login)
     plt.show()
 
 
-def analyse_user(elastic_search: Elasticsearch, user_login: str):
-    merges_performed_dataframe: pd.DataFrame = get_merges_performed(elastic_search, user_login)
-    merge_requests_dataframe: pd.DataFrame = get_merge_requests(elastic_search, user_login)
-    requests_merged_dataframe: pd.DataFrame = get_requests_merged(elastic_search, user_login)
+def check_stationarity(consolidated_dataframe: pd.DataFrame, user_login: str, data_column: str,
+                       threshold: float = 0.05):
+    test_result: list[float] = adfuller(consolidated_dataframe[data_column])
+    adf_statistic: float = test_result[0]
+    p_value: float = test_result[1]
+    if p_value <= threshold:
+        print("%s is stationary for user %s. ADF statistic: %f, p-value: %f" % (
+            data_column, user_login, adf_statistic, p_value))
+        return True
+
+    print("%s is NOT stationary for user %s. ADF statistic: %f, p-value: %f" % (
+        data_column, user_login, adf_statistic, p_value))
+    return True
+
+
+def analyse_user(es: Elasticsearch, user_login: str):
+    merges_performed_dataframe: pd.DataFrame = get_merges_performed(es, user_login)
+    merge_requests_dataframe: pd.DataFrame = get_merge_requests(es, user_login)
+    requests_merged_dataframe: pd.DataFrame = get_requests_merged(es, user_login)
 
     consolidated_dataframe: pd.DataFrame = pd.concat([merges_performed_dataframe, merge_requests_dataframe,
                                                       requests_merged_dataframe], axis=1)
     consolidated_dataframe = consolidated_dataframe.fillna(0)
+    print("Data points for user %s: %d" % (user_login, len(consolidated_dataframe)))
+    consolidated_dataframe = consolidated_dataframe.diff()
+    consolidated_dataframe = consolidated_dataframe.dropna()
+    print("Applying 1st order differencing to the data")
 
     if MERGES_PERFORMED_COLUMN in consolidated_dataframe and MERGES_SUCCESSFUL_COLUMN in consolidated_dataframe:
         correlation: float = consolidated_dataframe[MERGES_PERFORMED_COLUMN].corr(
             consolidated_dataframe[MERGES_SUCCESSFUL_COLUMN])
-        print(user_login, "Correlation: Merges done and succesful PRs ", correlation)
-        # plot_offered_vs_given(consolidated_dataframe, user_login)
+        print(user_login, "Correlation: Merges done and successful PRs ", correlation)
+        plot_offered_vs_given(consolidated_dataframe, user_login)
+        check_stationarity(consolidated_dataframe, user_login, MERGES_SUCCESSFUL_COLUMN)
+        check_stationarity(consolidated_dataframe, user_login, MERGES_PERFORMED_COLUMN)
 
     plot_dataframe(consolidated_dataframe, user_login)
 
 
 def main():
     aggregation_name: str = "frequent_mergers"
-    elastic_search: Elasticsearch = elasticsearch.Elasticsearch(ELASTICSEARCH_HOST)
-    search_results: dict[str, dict] = elastic_search.search(index=PULL_REQUEST_INDEX,
-                                                            size=0,
-                                                            aggs={
-                                                                aggregation_name: {
-                                                                    "terms": {
-                                                                        "field": "merged_by.login.keyword"
-                                                                    }
-                                                                }
-                                                            })
+    es: Elasticsearch = elasticsearch.Elasticsearch(ELASTICSEARCH_HOST)
+
+    es.indices.refresh(index=PULL_REQUEST_INDEX)
+    document_count: List[dict] = es.cat.count(index=PULL_REQUEST_INDEX, params={"format": "json"})
+    print("Documents on index %s: %s" % (PULL_REQUEST_INDEX, document_count[0]['count']))
+
+    search_results: dict[str, dict] = es.search(index=PULL_REQUEST_INDEX,
+                                                size=0,
+                                                aggs={
+                                                    aggregation_name: {
+                                                        "terms": {
+                                                            "field": "merged_by.login.keyword"
+                                                        }
+                                                    }
+                                                })
     mergers: List[str] = [merger_data['key'] for merger_data in
                           search_results['aggregations'][aggregation_name]['buckets']]
-    # user_login: str = mergers[0]
     for user_login in mergers:
-        analyse_user(elastic_search, user_login)
+        analyse_user(es, user_login)
 
 
 if __name__ == "__main__":
